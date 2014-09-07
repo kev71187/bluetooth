da854ddbbce1bf2c1ae7bfbffb3b8650b8d98f85
kevin woods <kevin@thebitstillery.com>
2014-09-06 19:23:57 -0700

    kw - bluetooth demo

--- app.js
+++ app.js
@@ -1,59 +1,26 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+btSerial.on('found', function(address, name) {
+    btSerial.findSerialPortChannel(address, function(channel) {
+        btSerial.connect(address, channel, function() {
+            console.log('connected');
 
-var app = express();
+            btSerial.write(new Buffer('my data', 'utf-8'), function(err, bytesWritten) {
+                if (err) console.log(err);
+            });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(favicon());
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-/// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-/// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
+            btSerial.on('data', function(buffer) {
+                console.log(buffer.toString('utf-8'));
+            });
+        }, function () {
+            console.log('cannot connect');
         });
-    });
-}
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
+        // close the connection when you're ready
+        btSerial.close();
+    }, function() {
+        console.log('found nothing');
     });
 });
 
-
-module.exports = app;
+btSerial.inquire();
